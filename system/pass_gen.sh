9e7d618327f1bff7101dc6e81be38eae915ad3d8
Mark Haferkamp <github.com@refola.com>
2015-05-10 03:22:30 -0700

    add simple password generator

---
+++ system/pass_gen.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Generate random password-suitable symbols from /dev/random
+
+if [ -z "$1" ]
+then
+	PATTERN="a-zA-Z0-9-_!@#$%^&*()+{}|:<>?='\`\"\\\\"
+else
+	PATTERN="$1"
+fi
+echo "Password character pattern is: $PATTERN"
+
+FILE="/tmp/my_temp_random"
+SEC="13"
+
+echo "Generating \"random\" bytes for $SEC seconds."
+cat /dev/random >> "$FILE" &
+pid="$!"
+sleep "$SEC"
+kill $pid
+# Use Bash builtin to help suppress "terminated" messages, per
+# https://stackoverflow.com/questions/81520/how-to-suppress-terminated-message-after-killing-in-bash
+wait $pid 2> /dev/null
+
+echo "Here are the \"random\" symbols."
+cat "$FILE" | tr -dc "$PATTERN" | fold -w 20
+echo
+
+echo "Now deleting temporary file $FILE."
+rm "$FILE"
