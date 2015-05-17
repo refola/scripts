5871daca116422689a9b7ac337d78252d8fba3da
Mark Haferkamp <github.com@refola.com>
2015-05-16 17:39:28 -0700

    reflow text

--- system/update-grub.sh
+++ system/update-grub.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-echo "\"update-grub\" probably doesn't exist on this system. It's running $MACHTYPE, not Ubuntu!"
-echo "This script is just a convenience to run \"grub2-mkconfig -o /boot/grub2/grub.cfg\" for you."
-echo "To keep it from being too convenient, you still have to get root your own way for update-grub.sh."
-echo
+echo "\"update-grub\" probably doesn't exist on this system. It's running
+${MACHTYPE}, not Ubuntu! This script is just a convenience to run
+\"grub2-mkconfig -o /boot/grub2/grub.cfg\" for you. To keep it from
+being too convenient, you still have to get root your own way for
+update-grub.sh."
+
 grub2-mkconfig -o /boot/grub2/grub.cfg
