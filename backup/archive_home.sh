f293a25d7580c1db80621ba718ce42dfc8bac999
Mark Haferkamp <github.com@refola.com>
2015-05-16 17:39:40 -0700

    delete old scripts whose functionality is batter handled by new ones

--- backup/archive_home.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Running \"archive home\" script"
-echo "Archiving home folder as \"home.tar.gz\""
-tar -cvzf home.tar.gz ~
-exit
--- backup/old/usage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Echos the syntax for the calling backup script
-# Syntax: $HERE/usage.sh name_of_backup_script rsync_backup_action
-# I hope the variables work from variables.sh which should've been called before.
-echo "Usage: $1 suffix"
-echo "This uses rsync to backup $2."
-echo "A list of locations in \"$AWAY\" follows."
-ls -A $AWAY
