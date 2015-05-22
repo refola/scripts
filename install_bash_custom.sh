37e820ddbeefb2e39b7fed835a061ca94377117b
Mark Haferkamp <github.com@refola.com>
2015-05-22 09:56:52 +0000

    install script for custom Bash environment

---
+++ install_bash_custom.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Install custom Bash profile stuff.
+
+# Figure out where the script is
+HERE="$(dirname "$(readlink -f "$0")")"
+
+# Backup old dot-files and replace them with symlinks to the ones here
+for file in .bash_logout .bash_profile .bashrc .logout .profile
+do
+    mv "$HOME/$file" "$HOME/$file.bak"
+    ln -s "$HERE/bash_custom/$file" "$HOME/$file"
+done
+
+# Make custom bash history directory for use with the custom $PROMPT_COMMAND and hist.sh
+mkdir "$HOME/.bash_history.d"
