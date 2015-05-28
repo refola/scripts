8639a4ad4aaba3dc89a84bbd2bbdd7d318b285c2
Mark Haferkamp <github.com@refola.com>
2015-05-28 00:57:13 -0700

    more shellchecking

--- screen/lights-off.sh
+++ screen/lights-off.sh
@@ -7,12 +7,12 @@ lightson
 stop() {
     # TODO: check if it's running before trying to kill it.
     echo "Stopping \"$1\" script."
-    killall --user `whoami` $1
+    killall --user "$USER" "$1"
 }
 
 for name in $names
 do
-    stop $name
+    stop "$name"
 done
 
 exit
--- screen/redshift-control.sh
+++ screen/redshift-control.sh
@@ -21,14 +21,15 @@ default_temp_night=55
 
 if [ -z "$1" ]
 then
-    echo "Usage: `basename $0` - | [(day)temp100s [nighttemp100s]] [(day)tint [nighttint]]"
+    local name="$(basename "$0")"
+    echo "Usage: $name - | [(day)temp100s [nighttemp100s]] [(day)tint [nighttint]]"
     echo "Kills currently running instances of redshift and runs it again with the given (day and night) color temperature(s) in hundreds of Kelvins."
     echo "  Temperature ranges from 10 through 100, correspoding to the 1000K to 10000K range."
     echo "  Tint ranges from 0 to 9, with \"0\" actually meaning \"10\" since 10 is a temperature, not a tint. Lower number meanings give darker screens."
     echo
-    echo "  Defaults: \"`basename $0` -\" is equivalent to \"`basename $0` ${default_tint_day} ${default_tint_night} ${default_temp_day} ${default_temp_night}\"."
+    echo "  Defaults: \"$name -\" is equivalent to \"$name $default_tint_day $default_tint_night $default_temp_day $default_temp_night\"."
     echo "  Note: It doesn't matter which order arguments are passed in."
-    echo "  Note: Location is currently hardcoded to ${place} with coordinates ${coords}. See the source to change this."
+    echo "  Note: Location is currently hardcoded to $place with coordinates $coords. See the source to change this."
     exit 1
 fi
 
@@ -37,7 +38,7 @@ killall redshift
 
 # Convert redcontrol tint values into redshift tint values
 tint() {
-    if (( $1 == 0 ))
+    if (( "$1" == 0 ))
     then
 	echo "1"
     else
@@ -83,28 +84,28 @@ do
 done
 
 # Gracefully handle any combination of unset vars.
-if [ -z $tint_day ]
+if [ -z "$tint_day" ]
 then
-    tint_day=$default_tint_day
-    tint_night=$default_tint_night
+    tint_day="$default_tint_day"
+    tint_night="$default_tint_night"
 fi
-if [ -z $tint_night ]; then tint_night=$tint_day; fi
-if [ -z $temp_day ]
+if [ -z "$tint_night" ]; then tint_night="$tint_day"; fi
+if [ -z "$temp_day" ]
 then
-    temp_day=$default_temp_day
-    temp_night=$default_temp_night
+    temp_day="$default_temp_day"
+    temp_night="$default_temp_night"
 fi
-if [ -z $temp_night ]; then temp_night=$temp_day; fi
+if [ -z "$temp_night" ]; then temp_night="$temp_day"; fi
 
 # Convert to real values
-temp_day=`temp $temp_day`
-temp_night=`temp $temp_night`
-tint_day=`tint $tint_day`
-tint_night=`tint $tint_night`
+temp_day="$(temp "$temp_day")"
+temp_night="$(temp "$temp_night")"
+tint_day="$(tint "$tint_day")"
+tint_night="$(tint "$tint_night")"
 
-echo "Starting redshift for ${place} at lat:lon ${coords} with (daytemp, nighttemp, daytint, nighttint) = (${temp_day}K, ${temp_night}K, ${tint_day}, ${tint_night})."
-echo "redshift -g ${gamma} -l ${coords} -t \"${temp_day}:${temp_night}\" -b \"${tint_day}:${tint_night}\" -r &"
-redshift -g ${gamma} -l ${coords} -t "${temp_day}:${temp_night}" -b "${tint_day}:${tint_night}" -r &
+echo "Starting redshift for $place at lat:lon $coords with (daytemp, nighttemp, daytint, nighttint) = (${temp_day}K, ${temp_night}K, $tint_day, $tint_night)."
+echo "redshift -g \"$gamma\" -l \"$coords\" -t \"$temp_day:$temp_night\" -b \"$tint_day:$tint_night\" -r &"
+redshift -g "$gamma" -l "$coords" -t "$temp_day:$temp_night" -b "$tint_day:$tint_night" -r &
 sleep 0.2 && echo # Make the prompt happen correctly after running this.
 
 exit
