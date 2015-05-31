2c90705f00f4245acf25181f8de0359baec76aa8
Mark Haferkamp <github.com@refola.com>
2015-05-30 19:57:17 -0700

    simplify logic by assuming it's properly installed

--- shortcut/cdh.sh
+++ shortcut/cdh.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-# NOTE: This must be sourced order to work!
-
 if [ -z "$1" ]
 then
-    if [ "$0" = "/bin/bash" ] # If this script is sourced as intended,
-    then
-	CMD="$(cmpath cdh)"   # then get the command's path via cmpath,
-    else
-	CMD="$0"              # Otherwise just use $0.
-    fi
-    echo "Usage: . $CMD path"
+    # This assumes that cdh is a function that sources this script.
+    echo "Usage: cdh path"
     echo "Changes the working directory to the given path"
     echo "and sets \$H to the path."
 else
     # Change to the given path and set $H to the path.
     cd "$1"
-    export H="$PWD" # Use $PWD instead of $1 
+    export H="$PWD" # $PWD gets rid of relative paths.
 fi
--- shortcut/cmcd.sh
+++ shortcut/cmcd.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# NOTE: This must be sourced order to work!
-
 if [ -z "$1" ]
 then
-    if [ "$0" = "/bin/bash" ] # If this script is sourced as intended,
-    then
-	CMD="$(cmpath cmcd)"  # then get the command's path via cmpath,
-    else
-	CMD="$0"              # Otherwise just use $0.
-    fi
-    echo "Usage: . $CMD command"
+    # This assumes that cmcd is a function that sources this script.
+    echo "Usage: cmcd command"
     echo "Changes the working directory to command's location."
 else
     # Get command's path, take directory name,
