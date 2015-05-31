efb1baa5b6ba4ad851b369eb1cd0ed96c90e8958
Mark Haferkamp <github.com@refola.com>
2015-05-31 03:16:02 -0700

    add parameter for time to wait between increments

--- volume/smooth-volume-up.sh
+++ volume/smooth-volume-up.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-echo "Running smooth volume-increasing script"
-for ((x=1; x<20; x++));
+echo "Running smooth volume-increasing script."
+
+SEC="9"
+
+if [ -z "$1" ]
+then
+    echo "You can pass this how many seconds to wait between increments."
+else
+    SEC="$1"
+fi
+
+for ((x=1; x<=20; x++));
 do
     echo $x/20
     volume-up > /dev/null
-    sleep 9
+    sleep "$SEC"
 done
+
 exit
