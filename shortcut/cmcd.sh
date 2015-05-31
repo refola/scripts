be45ad203213064e7f17e585f0f76c8490c9604b
Mark Haferkamp <github.com@refola.com>
2015-05-30 19:46:00 -0700

    merged function-to-sourced-script parts of cdh and cmcd to "shortcuts"

--- shortcut/cmcd.sh
+++ shortcut/cmcd.sh
@@ -2,22 +2,18 @@
 
 # NOTE: This must be sourced order to work!
 
-# This script is really more useful for being called within
-# scripts. For interactive shell users, the cmcd command in
-# ../sourced/cmcd.sh is more appropriate.
-
 if [ -z "$1" ]
 then
-    if [ "$0" = "/bin/bash" ]    # If this script is sourced as intended,
+    if [ "$0" = "/bin/bash" ] # If this script is sourced as intended,
     then
-	CMD="$(cmpath cmcd)" # then get the command's path via cmpath,
+	CMD="$(cmpath cmcd)"  # then get the command's path via cmpath,
     else
-	CMD="$0"             # but otherwise just use $0.
+	CMD="$0"              # Otherwise just use $0.
     fi
     echo "Usage: . $CMD command"
     echo "Changes the working directory to command's location."
 else
-    # Convert any symlinks in path, take directory name,
+    # Get command's path, take directory name,
     # and move to there.
     cd "$(dirname "$(cmpath "$1")")"
 fi
