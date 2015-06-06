a07fb569cd8b0de0737ebfe473f2715bc9c58fc1
Mark Haferkamp <github.com@refola.com>
2015-06-05 22:38:35 -0700

    much cleaner and proper-er code

--- lib/get-config.sh
+++ lib/get-config.sh
@@ -6,142 +6,117 @@
 # Sanity check
 if ! which cmdir dirname > /dev/null
 then
-    echo "Could not find all commands.... Exiting."
+    echo "Could not find all required commands.... Exiting." >&2
     exit 1
 fi
 
 # This line assumes that this script is in a sibling directory of the
 # config directory.
-DEFAULTS_PREFIX="$(dirname "$(cmdir "$0")")/config"
+defaults_prefix="$(dirname "$(cmdir "$0")")/config"
 # Where the live configs are.
-CONFIG_PREFIX="$HOME/.config/refola/scripts"
+config_prefix="$HOME/.config/refola/scripts"
 
 # Here's how to invoke this script.
-USAGE="Usage: $(basename "$0") script_name/config_name [description]
+usage="Usage: $(basename "$0") script_name/config_name [description]
 
 Outputs the contents of the config for script_name/config_name if it
-exists. Otherwise copies the default (if found), prints the
-description (if given) to stderr, and prompts the user to edit the
-config with $EDITOR.
+exists. Otherwise copies the default (if found), shows the description
+(if given) to the user, and prompts the user to edit the config with
+\$EDITOR.
 
 Returns 0 if the config was successfully retrieved and non-zero if
 there was an unresolved error.
 
 Config paths are as follows.
-Defaults:     $DEFAULTS_PREFIX
-Live configs: $CONFIG_PREFIX"
+Defaults:     $defaults_prefix
+Live configs: $config_prefix"
 
-## Usage: get_config_if_exists script/config
-# Prints config if it exists, otherwise returns non-zero.
-get_config_if_exists() {
-    local CFG_PATH="$CONFIG_PREFIX/$1"
-    if [ -f "$CFG_PATH" ]
-    then
-	cat "$CFG_PATH"
-	return 0
-    else
-	return 1
-    fi
-}
+# These variables are set by main, but defined here for clarity.
+config_name=""
+config_description=""
+config_path=""
+default_config_path=""
 
-## Usage: copy_default_config script/config
+
 # Copies the default config if it exists, otherwise makes a blank
 # config.
 copy_default_config() {
-    local CFG="$1"
-    local FROM="$DEFAULTS_PREFIX/$CFG"
-    local TO="$CONFIG_PREFIX/$CFG"
     # Make sure the destination directory exists.
-    mkdir -p "$(dirname "$TO")"
-    if [ -f "$FROM" ]
+    mkdir -p "$(dirname "$config_path")"
+    if [ -f "$default_config_path" ]
     then
-	cp "$FROM" "$TO"
+	cp "$default_config_path" "$config_path"
     else
-	touch "$TO"
+	touch "$config_path"
     fi
 }
 
-## Usage: edit_config_prompt script/config description
-# Shows the user the path and description of the given config and asks
-# if the user wants to edit it.
-## Returns 0 for yes and non-zero for no.
-edit_config_prompt() {
-    local CFG="$1"
-    local CFG_PATH="$CONFIG_PREFIX/$CFG"
-    local DESC="$2"
-    if [ -n "$DESC" ]
-    then
-	echo "$CFG: $DESC"
+# Usage: color code text
+# Adds code to make text color.
+color() { echo -n "\e[${1}m${2}\e[0m"; }
+# Usage: yellow text
+# Adds code to makes text yellow.
+yellow() { color '1;33' "$1"; }
+
+# Explains to the user everything we know about the config.
+explain_config() {
+    if [ -n "$config_description" ]; then
+	echo -e "$config_name: $(yellow "$config_description")"
     fi
-    echo -e "$CFG has the following configuration:\n\e[1;33m$(cat "$CFG_PATH")\e[0m"
-    local ANS
-    read -n1 -p "Do you want to edit $CFG: [Yes]/[No] (default: no)? " ANS
-    echo
-    if [ "$ANS" = y -o "$ANS" = Y ]
-    then
+    if [ -f "$default_config_path" ]; then
+	echo "Here's the default configuration for $config_name."
+	echo -e "$(yellow "$(cat "$default_config_path")")"
+    else
+	echo "No default configuration found for $config_name. Going with blank config."
+    fi
+}
+
+# Checks if the configuration can be retrieved, interactively loading
+# defaults and prompting the user to edit the config as
+# appropriate. Returns 0 if the configuration appears to be
+# retrievable and non-zero if the script should be aborted.
+can_read_config() {
+    if [ -f "$config_path" ]; then
 	return 0
     else
-	return 1
-    fi
-}
-
-## Usage: edit_config script/config
-# Edits the given config with $EDITOR.
-edit_config() {
-    "$EDITOR" "$CONFIG_PREFIX/$1"
-}
-
-## Usage: abort_prompt
-# Asks the user if they want to abort.
-## Returns 0 for yes and non-zero for no.
-abort_prompt() {
-    local ANS
-    read -n1 -p "Do you want to abort this script and give up [Yes]/[No] (default: yes)? " ANS
-    echo
-    if [ -z "$ANS" -o "$ANS" = y -o "$ANS" = Y ]
-    then
+        explain_config
+	# Echo question separately because 'read' does not support color codes.
+	echo -e -n "[$(yellow A)]bort (default) / use [$(yellow D)]efault config / [$(yellow E)]dit config? "
+	local ans; read -n1 ans
+	echo # because 'read' doesn't
+	case "$ans" in
+	    [dD] ) copy_default_config
+		   ;;
+	    [eE] ) copy_default_config
+		   "$EDITOR" "$config_path"
+		   ;;
+	    *    ) echo -e "$(color '0;31' "$config_name has not been configured. The script will not run.")"
+		   return 1
+		   ;;
+	esac
 	return 0
-    else
-	return 1
     fi
 }
 
-## Usage: get_config script/config description
-# Gets the requested config, copying the default and prompting the
-# user for editing as required.
-get_config() {
-    # WARNING: Everything here except get_config_if_exists must be
-    # '>&2'-ified to avoid returning the wrong config.
-    local CFG="$1"
-    local DESC="$2"
-    if ! get_config_if_exists "$CFG"
-    then
-	copy_default_config "$CFG" >&2
-	if edit_config_prompt "$CFG" "$DESC" >&2
-	then
-	    edit_config "$CFG" >&2
-	elif abort_prompt >&2
-	then
-	    echo -e "\e[01;37mWARNING: \e[00;31m$CFG has not been configured. The script will not run.\e[0m" >&2
-	    rm "$CONFIG_PREFIX/$CFG"
-	    return 1
-	fi
-	# By now the config must exist.
-	get_config_if_exists "$CFG"
-    fi
-    return 0
-}
-
-## Usage: main
+## Usage: main "$@"
 # Gets config iff valid, making sure to output non-config stuff to stderr.
 main() {
     if [ -z "$1" ]
     then
-	echo "$USAGE" >&2
+	echo "$usage" >&2
 	exit 1
     else
-	get_config "$1" "$2"
-	exit $?
+	config_name="$1"
+	config_description="$2"
+	config_path="$config_prefix/$config_name"
+	default_config_path="$defaults_prefix/$config_name"
+	if ! can_read_config >&2
+	then
+	    exit 1
+	else
+	    cat "$config_path"
+	    exit $?
+	fi
     fi
 }
 
