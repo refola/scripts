afb9fdfd29d872acdfb1e41f65cf2882bda32944
Mark Haferkamp <github.com@refola.com>
2015-06-08 04:05:51 -0700

    cache any folder requested, not just $PWD

--- filesystem/cache-folder.sh
+++ filesystem/cache-folder.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-echo "Running \"find . -type f -exec cat {} > /dev/null +\" to put everything in this folder into the kernel's filesystem cache."
+# Cache a single folder, either what's passed as an argument or the
+# current working directory.
+if [ -n "$1" ]
+then
+    cd "$1"
+else
+    echo "No folder passed, so caching \$PWD=$PWD."
+fi
+
+echo "Caching folder $PWD."
 find . -type f -exec cat {} > /dev/null +
-exit
