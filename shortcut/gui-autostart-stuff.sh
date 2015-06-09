94efebc071deb3332adb9b5c2a253a8982f9551c
Mark Haferkamp <github.com@refola.com>
2015-06-09 02:12:36 -0700

    move config to separate file

--- shortcut/gui-autostart-stuff.sh
+++ shortcut/gui-autostart-stuff.sh
@@ -1,18 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "This script is intended to auto-launch a bunch of programs that
 are wanted for a GUI computing session. If you are seeing this
 message, then you're doing it wrong. See your desktop environment's
 documentation for how to do it right."
 
-commands=(
-    #"godoc -http=:6060" # Go language documentation server
-    "redshift-control -" # Start Redshift with saved settings
-    #"cache-places" # Pre-cache a bunch of common stuff
-    "emacs --daemon" # Enable emacsclient for instantness
-)
+IFS=$'\n' # Separate commands by newlines.
+commands=( $(get-config "gui-autostart-stuff/commands" \
+                        "list of commands to start") )
 
 for cmd in "${commands[@]}"
 do
+    # Separate command and arguments with spaces.
+    IFS=' '
     $cmd
 done
