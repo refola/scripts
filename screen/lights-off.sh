6fbecf31c2130db077fabe8bdeecabdf36ec20aa
Mark Haferkamp <github.com@refola.com>
2015-06-10 12:04:29 -0700

    automatic name and error messages

--- screen/lights-off.sh
+++ screen/lights-off.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-names="
-lights-on.sh
-lights-on
-"
 
-stop() {
-    # TODO: check if it's running before trying to kill it.
-    echo "Stopping \"$1\" script."
-    killall --user "$USER" "$1"
-}
+this="$(basename "$0")"
+that="${this/off/on}"
 
-for name in $names
-do
-    stop "$name"
-done
+if [ "$this" = "$that" ]
+then
+    echo "Error: This script's name '$this' does not contain the string 'off'." >&2
+    echo "The lights-on and lights-off scripts must have corresponding names." >&2
+    exit 1
+fi
 
-exit
+# TODO: check if it's running before trying to kill it.
+echo "Stopping '$that'."
+if ! killall --user "$USER" "$that" 2>/dev/null
+then
+    echo "Error: Could not kill '$that'." >&2
+    exit 1
+fi
