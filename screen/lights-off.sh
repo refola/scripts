42695c0ab794401eec50cb177ef5b8376bdbda4d
Mark Haferkamp <github.com@refola.com>
2015-06-12 07:41:33 -0700

    now with check for if lights-on is running

--- screen/lights-off.sh
+++ screen/lights-off.sh
@@ -10,10 +10,15 @@ then
     exit 1
 fi
 
-# TODO: check if it's running before trying to kill it.
-echo "Stopping '$that'."
-if ! killall --user "$USER" "$that" 2>/dev/null
+pids="$(pidof -x "$that")" # Not portable, but neither is lights-on
+if [ "$?" = "0" ]
 then
-    echo "Error: Could not kill '$that'." >&2
+    for pid in $pids
+    do
+        echo "Stopping $that with pid=$pid."
+        kill $pid
+    done
+else
+    echo "Error: No process with name '$that'." >&2
     exit 1
 fi
