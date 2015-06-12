8414268d47ce706f679fa2745ae26c30648f859b
Mark Haferkamp <github.com@refola.com>
2015-06-12 07:42:22 -0700

    fallback to fresh instance if daemon doesn't work

--- shortcut/emacsclient-c.sh
+++ shortcut/emacsclient-c.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# Run "emacsclient -c" as a $VISUAL-compatible command.
-emacsclient -c "$@"
+# Run "emacsclient -c" as a $VISUAL-compatible command, falling back
+# to emacs-nox if needed.
+
+# Try connecting to existing emacs daemon.
+if ! (pidsof emacs && emacsclient -c "$@")
+then
+    # Otherwise fall back to emacs.
+    emacs "$@"
+fi
--- shortcut/emacsclient-t.sh
+++ shortcut/emacsclient-t.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-# Run "emacsclient -t" as a $VISUAL-compatible command.
-emacsclient -t "$@"
+# Run "emacsclient -t" as a $EDITOR-compatible command, falling back
+# to emacs-nox if needed.
+
+# Try connecting to existing emacs daemon.
+if ! (pidsof emacs && emacsclient -t "$@")
+then
+    # Otherwise fall back to emacs-nox.
+    emacs-nox "$@"
+fi
