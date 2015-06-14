d8d8f8f0c89357844695df20e5467a078eff8f93
Mark Haferkamp <github.com@refola.com>
2015-06-13 22:02:02 -0700

    don't break emacsclient by redirecting its output

--- shortcut/emacsclient-c.sh
+++ shortcut/emacsclient-c.sh
@@ -3,7 +3,9 @@
 # to emacs-nox if needed.
 
 # Try connecting to existing emacs daemon.
-if ! (pidsof emacs && emacsclient -c "$@") 2> /dev/null
+## Note: Redirecting emacsclient's output keeps it from getting the
+## terminal's name, so only suppress pidsof's output.
+if ! (pidsof emacs &> /dev/null && emacsclient -c "$@")
 then
     # Otherwise fall back to emacs.
     emacs "$@"
--- shortcut/emacsclient-t.sh
+++ shortcut/emacsclient-t.sh
@@ -3,8 +3,12 @@
 # to emacs-nox if needed.
 
 # Try connecting to existing emacs daemon.
-if ! (pidsof emacs && emacsclient -t "$@") 2> /dev/null
+## Note: Redirecting emacsclient's output keeps it from getting the
+## terminal's name, so only suppress pidsof's output.
+if ! (pidsof emacs &> /dev/null && emacsclient -t "$@")
 then
     # Otherwise fall back to emacs-nox.
+    echo "Could not connect to emacs daemon. Starting new instance."
     emacs-nox "$@"
+    echo "Try running 'emacs --daemon' for faster startup."
 fi
