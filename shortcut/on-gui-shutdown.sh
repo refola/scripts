25b118a2371002f9da25b0b8b0f0e022d1e0d4ad
Mark Haferkamp <github.com@refola.com>
2015-06-14 22:27:00 -0700

    use -var-rep and -what-do options in get-config

---
+++ shortcut/on-gui-shutdown.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "This script is intended to auto-launch a bunch of programs when
+stopping a GUI computing session. If you are seeing this message, then
+you're doing it wrong. See your desktop environment's documentation
+for how to do it right."
+
+IFS=$'\n' # Separate commands by newlines.
+commands=( $(get-config "on-gui-shutdown/commands" \
+                        -what-do "list of commands to run on shutdown" \
+                        -var-rep ) )
+
+for cmd in "${commands[@]}"
+do
+    # Show command being ran, in case this is being used in a debug
+    # context. Output is ignored anyway in the normal case.
+    echo "Command: $cmd"
+    # Actually run the command. Using eval seems cleaner than trying
+    # to run $cmd, especially if it's complex.
+    eval "$cmd"
+done
