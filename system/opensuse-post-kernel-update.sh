9a66bcc382d381e4b6553603864b0d98152b19ba
Mark Haferkamp <github.com@refola.com>
2015-07-15 13:07:23 -0700

    seperate package list and update to newer version

--- system/opensuse-post-kernel-update.sh
+++ system/opensuse-post-kernel-update.sh
@@ -2,5 +2,12 @@
 # Do stuff required after kernel updates to keep the system working properly
 
 # Fix nvidia drivers after kernel update on openSUSE
-sudo zypper in --force nvidia-glG03 nvidia-computeG03 nvidia-gfxG03-kmp-desktop nvidia-uvm-gfxG03-kmp-desktop x11-video-nvidiaG03
-
+nvidia_pkgs=(
+    nvidia-computeG04
+    nvidia-gfxG04-kmp-desktop
+    nvidia-glG04
+    nvidia-uvm-gfxG04-kmp-desktop
+    x11-video-nvidiaG04
+)
+echo "Forcing reinstallation of: ${nvidia_pkgs[*]}"
+sudo zypper in --force "${nvidia_pkgs[@]}"
