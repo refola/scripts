a26ed46f56d0d322b7905f292f07027b13f4d7ce
Mark Haferkamp <github.com@refola.com>
2015-07-15 11:13:39 -0700

    deduplicate

--- system/chroot-to-distro.sh
+++ system/chroot-to-distro.sh
@@ -3,19 +3,24 @@
 if [ -z "$1" ]
 then
     echo "Usage: $(basename "$0") mount-point"
-    echo "Sets things up, chroots into mount-point, and tares things down when done."
+    echo "Sets things up, chroots into mount-point, and tears things down when done."
     exit 1
 fi
 
-echo "Setting up chroot for $1."
-cd "$1"
-sudo mount -o bind /dev dev
-sudo mount -o bind /proc proc
-sudo mount -o bind /sys sys
+ch="$1"
+echo "Setting up chroot for $ch."
+cd "$ch"
+
+binds=( dev home proc run sys )
+for bind in "${binds[@]}"; do
+    echo "Binding $bind to $ch/$bind."
+    sudo mount -o bind "/$bind" "$ch/$bind"
+done
+
 sudo mount -t tmpfs tmpfs tmp
 
-echo "Chrooting into $1."
-sudo chroot "$1"
+echo "Chrooting into $ch."
+sudo chroot "$ch"
 
-echo "Undoing special mounts in $1."
-sudo umount dev proc sys tmp
+echo "Undoing special mounts in $ch."
+sudo umount "${binds[@]}" tmp
