dbb1722c8abd136fa2c13ceab1e4b3c275587928
Mark Haferkamp <github.com@refola.com>
2015-11-17 22:36:20 -0700

    immediately exit when passed invalid folder name

--- filesystem/cache-folder.sh
+++ filesystem/cache-folder.sh
@@ -4,6 +4,10 @@
 if [ -n "$1" ]
 then
     cd "$1"
+    if [ "$?" != "0" ]; then
+        echo "Folder doesn't exist. Aborting!"
+        exit 1
+    fi
 else
     echo "No folder passed, so caching \$PWD=$PWD."
 fi
