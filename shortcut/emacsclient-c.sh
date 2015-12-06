8344a03cb919b8d6842862db51a96d35f155a6c5
Mark Haferkamp <github.com@refola.com>
2015-12-05 18:25:22 -0700

    have emacsclient start the daemon if needed

--- shortcut/emacsclient-c.sh
+++ shortcut/emacsclient-c.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-# Run "emacsclient -c" as a $VISUAL-compatible command, falling back
-# to emacs-nox if needed.
-
-# Try connecting to existing emacs daemon.
-## Note: Redirecting emacsclient's output keeps it from getting the
-## terminal's name, so only suppress pidsof's output.
-if ! (pidsof emacs &> /dev/null && emacsclient -c "$@")
-then
-    # Otherwise fall back to emacs.
-    echo "Could not connect to emacs daemon. Trying to start new instance."
-    if emacs --daemon
-    then
-	echo "Emacs daemon started. Now trying to connect."
-	if ! emacsclient -c "$@"
-	then
-	    echo "Could not connect to freshly started emacs daemon."
-	    echo "Something's seriously wrong. Giving up."
-	fi
-    else
-	echo "Could not start emacs in daemon mode. Starting it in slow mode."
-	emacs "$@"
-	echo "Try running 'emacs --daemon' for faster startup."
-    fi
-fi
+# Run "emacsclient -c" ("create-frame") as a $VISUAL-compatible
+# command. '-a ""' tries to start a new daemon if one isn't running.
+emacsclient -c -a "" "$@"
--- shortcut/emacsclient-t.sh
+++ shortcut/emacsclient-t.sh
@@ -1,25 +1,4 @@
 #!/bin/bash
-# Run "emacsclient -t" as a $EDITOR-compatible command, falling back
-# to emacs-nox if needed.
-
-# Try connecting to existing emacs daemon.
-## Note: Redirecting emacsclient's output keeps it from getting the
-## terminal's name, so only suppress pidsof's output.
-if ! (pidsof emacs &> /dev/null && emacsclient -t "$@")
-then
-    # Otherwise fall back to emacs-nox.
-    echo "Could not connect to emacs daemon. Trying to start new instance."
-    if emacs --daemon
-    then
-	echo "Emacs daemon started. Now trying to connect."
-	if ! emacsclient -t "$@"
-	then
-	    echo "Could not connect to freshly started emacs daemon."
-	    echo "Something's seriously wrong. Giving up."
-	fi
-    else
-	echo "Could not start emacs in daemon mode. Starting it in slow mode."
-	emacs-nox "$@"
-	echo "Try running 'emacs --daemon' for faster startup."
-    fi
-fi
+# Run "emacsclient -t" (terminal mode) as a $EDITOR-compatible
+# command. '-a ""' tries to start a new daemon if one isn't running.
+emacsclient -t -a "" "$@"
