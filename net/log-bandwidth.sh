d5679a2b4a290ae597f69883a9ba21761476b04d
Mark Haferkamp <github.com@refola.com>
2015-12-24 03:45:54 -0700

    use "ip" instead of "ifconfig", and use functions

--- net/log-bandwidth.sh
+++ net/log-bandwidth.sh
@@ -12,11 +12,31 @@ interfaces=( $(get-config "log-bandwidth/interfaces" \
 mkdir -p "$(dirname "$log_location")"
 touch "$log_location"
 
-date -Iseconds >> "$log_location"
+## Usage: log [options] [stuff to log]
+# Logs stuff to $log_location. Accepts the same arguments as echo.
+log() {
+    echo "$@" >> "$log_location"
+}
+
+## Usage: get-bytes interface direction
+# Gets the number of bytes of bandwidth use for interface in
+# direction, since "ip" doesn't have custom output format
+# control. Direction can be RX or TX.
+get-bytes() {
+    local if="$1"
+    local dir="$2"
+    ip -s link show "$if" | \ # Get stats
+        grep -E -A1 "^ *$dir: bytes .*\$" | \ # Find right line
+        tail -n1 | \ # Strip preceding wrong line
+        grep -E -o -m1 "[0-9]+" | \ # Get just the nmubers
+        head -n1 # Limit to first match (byte count)
+}
+
+log "$(date -Iseconds)"
 for interface in "${interfaces[@]}"; do
     for direction in RX TX; do
-        echo -n "$interface: $direction " >> "$log_location"
-        ifconfig "$interface" | grep "$direction packets" | sed 's/ *[RT]X packets [0-9]* *//' >> "$log_location"
+        log -n "$interface $direction: "
+        log "$(get-bytes "$interface" "$direction") bytes"
     done
 done
-echo >> "$log_location"
+log # newline
