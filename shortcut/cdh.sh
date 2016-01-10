d62012ea0fd68efc0a4ad9650591c35bbf0eeca9
Mark Haferkamp <github.com@refola.com>
2016-01-09 17:56:05 -0700

    now also a shortcut for "cd $H"

--- shortcut/cdh.sh
+++ shortcut/cdh.sh
@@ -2,10 +2,16 @@
 
 if [ -z "$1" ]
 then
-    # This assumes that cdh is a function that sources this script.
-    echo "Usage: cdh path"
-    echo "Changes the working directory to the given path"
-    echo "and sets \$H to the path."
+    if [ "$PWD" = "$H" ]
+    then
+        # This assumes that cdh is a function that sources this script.
+        echo "Usage: cdh [path]"
+        echo "Changes the working directory to the given path"
+        echo "and sets \$H to the path. Otherwise changes the"
+        echo "working directory to \$H."
+    else
+        cd "$H"
+    fi
 else
     # Change to the given path and set $H to the path.
     cd "$1"
