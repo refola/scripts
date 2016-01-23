694ed22962d68706b9f3c865966093e22daf2e9b
Mark Haferkamp <github.com@refola.com>
2016-01-23 15:06:51 -0700

    don't log interfaces that don't exist

--- net/log-bandwidth.sh
+++ net/log-bandwidth.sh
@@ -28,23 +28,31 @@ get-bytes() {
     local result
     # Get stats
     result="$(ip -s link show "$if")"
-    # Find right line
-    result="$(echo "$result" | grep -E -A1 "^ *$dir: bytes .*\$")"
-    # Strip preceding wrong line
-    result="$(echo "$result" | tail -n1)"
-    # Get just the numbers
-    result="$(echo "$result" | grep -E -o -m1 "[0-9]+")"
-    # Limit to first match (byte count)
-    result="$(echo "$result" | head -n1)"
-    # Finally output result
-    echo "$result"
+    if [ "$?" = "0" ]; then
+        # Find right line
+        result="$(echo "$result" | grep -E -A1 "^ *$dir: bytes .*\$")"
+        # Strip preceding wrong line
+        result="$(echo "$result" | tail -n1)"
+        # Get just the numbers
+        result="$(echo "$result" | grep -E -o -m1 "[0-9]+")"
+        # Limit to first match (byte count)
+        result="$(echo "$result" | head -n1)"
+        # Finally output result
+        echo "$result"
+        return 0
+    else
+        return 1
+    fi
 }
 
 log "$(date -Iseconds)"
 for interface in "${interfaces[@]}"; do
     for direction in RX TX; do
-        log -n "$interface $direction: "
-        log "$(get-bytes "$interface" "$direction") bytes"
+        bytes="$(get-bytes "$interface" "$direction")"
+        if [ "$?" = "0" ]; then
+            log -n "$interface $direction: "
+            log "$bytes bytes"
+        fi
     done
 done
 log # newline
