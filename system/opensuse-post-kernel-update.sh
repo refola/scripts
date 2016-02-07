cd4a3b417c3e9f6f65e4d000e20aba5e439187a2
Mark Haferkamp <github.com@refola.com>
2016-02-07 04:15:31 -0700

    openSUSE Leap 42.1 update, but may be obsolete

--- system/opensuse-post-kernel-update.sh
+++ system/opensuse-post-kernel-update.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 # Do stuff required after kernel updates to keep the system working properly
 
+echo "This script might not be necessary since openSUSE 42.1 Leap."
+
 # Fix nvidia drivers after kernel update on openSUSE
 nvidia_pkgs=(
     nvidia-computeG04
-    nvidia-gfxG04-kmp-desktop
+    nvidia-gfxG04-kmp-default
     nvidia-glG04
-    nvidia-uvm-gfxG04-kmp-desktop
+    nvidia-uvm-gfxG04-kmp-default
     x11-video-nvidiaG04
 )
 echo "Forcing reinstallation of: ${nvidia_pkgs[*]}"
