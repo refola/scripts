6fd0e6521bc2768b196c3c299ba57ec8623f6be9
Mark Haferkamp <github.com@refola.com>
2016-03-12 03:08:58 -0700

    maybe more reliable?

--- system/restart-kde5-desktop.sh
+++ system/restart-kde5-desktop.sh
@@ -1,11 +1,27 @@
 #!/bin/bash
 
+## Usage: run-maybe-5 program args ...
+# Runs program5 if it exists, or program if program5 doesn't exist,
+# passing along given args.
+run-maybe-5() {
+    local prog="$1"
+    shift
+    if which "${prog}6" 2>/dev/null; then
+        prog="${prog}5"
+    fi
+    $prog "$@"
+}
+
+## Usage: restart app [start-args ...]
+# Uses kquitapp(5) and kstart(5) to stop and start the given app,
+# using the given args on start.
+restart() {
+    echo "Restarting $1."
+    run-maybe-5 kquitapp "$1"
+    run-maybe-5 kstart "$@"
+}
+
 echo "Restarting 'KDE5' desktop stuff."
-
-for app in kwin_x11 plasmashell; do
-    echo "Restarting $app."
-    kquitapp "$app"
-    kstart "$app"
-done
-
+restart kwin_x11 --replace
+restart plasmashell
 echo "Done."
