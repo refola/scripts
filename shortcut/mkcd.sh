09c7e320af03cc1d5a829f6147f00523fca016f7
Mark Haferkamp <github.com@refola.com>
2016-04-03 18:01:13 -0700

    use "-p" on mkdir

--- shortcut/mkcd.sh
+++ shortcut/mkcd.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # This assumes that mkcd is a function that sources this script.
-local usage="Usage: mkcd relative/path/to/folder
+local usage="Usage: mkcd possibly-relative/path/to/folder
 
 Makes the given folder if it doesn't already exist and changes the
-working directory to it. This is equivalent to calling 'mkdir [path]'
-and then 'cd [path]'."
+working directory to it. This is equivalent to calling 'mkdir -p path'
+and then 'cd path'."
 
 if [ "$#" != "1" ]
 then
     echo "$usage"
 else
-    mkdir "$1"
+    mkdir -p "$1"
     cd "$1"
 fi
