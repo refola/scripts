3d6c1c23133dada1b043066a262573757c9784a3
Mark Haferkamp <github.com@refola.com>
2016-04-06 19:01:48 -0700

    script to monitor remaining RAM and alert if low

---
+++ system/ram-monitor.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+while true; do
+    # get amount of free memory
+    available="$(grep MemAvailable /proc/meminfo | egrep -o '[0-9]+')"
+    if [ $? != 0 ]; then
+        echo "$0: Could not get available memory." | write "$USER"
+        sleep 10
+    fi
+    # convert from KB to MB
+    available_mb=$((available/1000))
+    # check that there's at least 3 GB left
+    if [ "$available_mb" -lt "3000" ]; then
+        echo "Low memory! We're down to $available_mb MB!" | write "$USER"
+        sleep 10
+    fi
+    sleep 0.1
+done
