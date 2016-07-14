83789216724b6da2048b6b2bbb1e4fdf69d42616
Mark Haferkamp <github.com@refola.com>
2016-07-14 11:51:16 -0700

    quieter and faster where supported

--- system/fix-sound.sh
+++ system/fix-sound.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
-echo "Using the 'say' command to start-and-stop an audio stream twice,"
+echo "Muting sound."
+volume-mute
+
+word="yh" # "yh" seems to be the shortest-time sound it can do
+cmd=(say "$word")
+
+
+if [ -e "$(which spd-say)" ]; then # use lower-level parameters to be faster
+    cmd=(spd-say --wait --rate +100 "$word")
+fi
+
+echo "Using '${cmd[0]}' to start-and-stop an audio stream twice,"
 echo "which seems to fix cracking audio after changing sessions."
-say yh
-say yh
+${cmd[@]}
+${cmd[@]}
+
+echo "Unmuting sound."
+volume-mute
