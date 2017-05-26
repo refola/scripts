27bc73ec50acf8bbadf5f396ccd8e4046b5e2d81
Mark Haferkamp <refola@users.noreply.github.com>
2017-05-25 22:36:41 -0700

    nicer usage and spd-say dependency messages

--- speech/say.sh
+++ speech/say.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Usage: $(basename "$0") words that you want spoken"
-    echo "Uses a speech synthesizer to speak words that you want spoken."
+    echo "Usage: $(basename "$0") words [...]"
+    echo "Uses a speech synthesizer to speak the given words."
     exit 1
 fi
 
 if [ ! -e "$(which spd-say)" ]; then
-    echo "Error: This command depends on 'spd-say' command."
+    echo -e "Error: Could not find \e[1mspd-say\e[0m command."
     pkg="speech-dispatcher"
-    echo "Now searching for a '$pkg' package."
+    echo -e "Searching for \e[1m$pkg\e[0m package to get \e[1mspd-say\e[0m."
     if pm s "$pkg"; then
-        echo "Run 'pm in [package-name]' to install a package."
+        echo -e "Please run \e[1mpm in \e[3mpackage-name\e[0m to install the right package."
     fi
     exit 1
 fi
