ba3401c7c5cf8d110df18165cbaf8be0b24cc730
Mark Haferkamp <refola@users.noreply.github.com>
2017-09-11 13:20:05 -0700

    add character frequency analysis script

---
+++ example/char-frequency.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+declare -A chars
+msg="$1"
+len="${#msg}"
+i=0
+
+inc(){
+    local x="${chars[$1]}"
+    ((x++))
+    chars[$1]="$x"
+}
+
+while [ "$i" -lt "$len" ]; do
+    char="${msg:$i:1}"
+    inc "$char"
+    ((i++))
+done
+
+for i in "${!chars[@]}"; do
+    echo "$i: ${chars[$i]}"
+done
