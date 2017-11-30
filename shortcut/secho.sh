48d0d9f605418efee72736facf82d3fc48ca5de9
Mark Haferkamp <refola@users.noreply.github.com>
2017-11-29 20:14:07 -0700

    clearer usage, multiple files

--- shortcut/secho.sh
+++ shortcut/secho.sh
@@ -2,14 +2,26 @@
 ## secho.sh
 # Do what is naively intended by the most common `sudo echo...` commands.
 
-usage="Usage: secho text file
+usage="Usage: secho text file [...]
 
-This is a shortcut for 'echo text | sudo tee file >/dev/null', and
-named after the 'sudo echo' in the (wrong) 'sudo echo text > file'."
+Do what may be naively intended by 'sudo echo text > file'. In
+particular, this is a shortcut for:
 
-if [ -z "$2" ] || [ -n "$3" ]; then
+    echo text | sudo tee file1 >/dev/null
+
+As a bonus, you can pass additional file names and they will also be
+written to with sudo, equivalent to
+
+    echo text | sudo tee file1 | sudo tee file2 [...] >/dev/null"
+
+if [ -z "$2" ]; then
     echo "$usage"
     exit 1
 else
-    echo "$1" | sudo tee "$2" >/dev/null
+    text="$1"
+    shift
+    while [ -n "$1" ]; do
+        echo "$text" | sudo tee "$1" >/dev/null
+        shift
+    done
 fi
