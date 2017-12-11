83d87571ba1f7822644d69d89d53d53118e2a56c
Mark Haferkamp <refola@users.noreply.github.com>
2017-12-10 19:03:48 -0700

    make --chdir destination as stable as expected

--- emacs/start-emacs.sh
+++ emacs/start-emacs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## start-emacs.sh
 
-cmd=(emacs --daemon --chdir "$H")
+cmd=(emacs --daemon --chdir "/home/$USER")
 
-# If not (Emacs is running), then start a new daemon.
+# If the current user isn't running Emacs, then start a new daemon.
 if ! pgrep -u "$USER" -f "${cmd[*]}" >/dev/null; then
     "${cmd[@]}"
 fi
