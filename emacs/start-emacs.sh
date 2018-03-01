edf234f5a3d814d17ec4f3edba44d999d63d43fd
Mark Haferkamp <refola@users.noreply.github.com>
2018-02-28 17:47:58 -0700

    should be more compatible with Termux (need "procps" package)

--- emacs/start-emacs.sh
+++ emacs/start-emacs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ## start-emacs.sh
 
-cmd=(emacs --daemon --chdir "/home/$USER")
+cmd=(emacs --daemon --chdir "${H-$HOME}")
 
 # If the current user isn't running Emacs, then start a new daemon.
-if ! pgrep -u "$USER" -f "${cmd[*]}" >/dev/null; then
+if ! pgrep --euid "$EUID" --full "${cmd[*]}" >/dev/null; then
     "${cmd[@]}"
 fi
