eb85cdeb60e2f287112e40800cefc9ae9bf6b0b3
Mark Haferkamp <refola@users.noreply.github.com>
2018-02-28 18:08:18 -0700

    make it deterministic again
    Yes, I know this is rather roundabout compared to just using `$HOME`
    directly. But I have a separate `$HOME` for each distro, all living
    under `/home/$USER`. This is less annoying than dealing with version
    incompatibilities between config files for various programs, many of
    which I don't even want to think about configuration of.

--- emacs/start-emacs.sh
+++ emacs/start-emacs.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 ## start-emacs.sh
 
-cmd=(emacs --daemon --chdir "${H-$HOME}")
+start_dir="/home/$USER"
+if [ ! -d "$start_dir" ] || [ -z "$USER" ]; then
+    start_dir="$HOME"
+fi
+
+cmd=(emacs --daemon --chdir "$start_dir")
 
 # If the current user isn't running Emacs, then start a new daemon.
 if ! pgrep --euid "$EUID" --full "${cmd[*]}" >/dev/null; then
