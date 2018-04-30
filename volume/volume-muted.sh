3ab5db733f35a4264d5962d41426a86b090221da
Mark Haferkamp <refola@users.noreply.github.com>
2018-04-29 21:16:01 -0700

    fix not outputting mute-check result

--- volume/volume-muted.sh
+++ volume/volume-muted.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 if [ "$1" != "-q" ]; then
-    echo -n "Is volume muted? "
-    pactl list sinks | grep -q 'Mute:' | cut -c8-
+    if pactl list sinks | grep -q 'Mute: yes'; then
+        echo "Volume is muted."
+    else
+        echo "Volume is not muted."
+    fi
     echo
     echo "Pass '-q' for quiet (status code only) checking."
 fi
