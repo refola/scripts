47e6b721a0a670f80da44fff9e08ae2da00764bc
Mark Haferkamp <refola@users.noreply.github.com>
2018-12-30 20:05:39 -0700

    Add '-f' switch to force path existence before 'cd'

--- shortcut/cdl.sh
+++ shortcut/cdl.sh
@@ -2,18 +2,33 @@
 ## cdl.sh
 # Sourcable script for `cdl()` function.
 
+# This is sourced into a function, so "local" is valid.
+# shellcheck disable=SC2168
+local force path h pwd
+
+if [ "$1" = "-f" ]; then
+    force=true
+    shift
+fi
+
 # Get place to load variables from.
 path="$(get-config "var-save/$*" -path)"
 
 if [ -z "$1" ] || [ ! -e "$path" ]; then
-    echo "Usage: cdl name"
+    echo "Usage: cdl [-f] name"
     echo "Restores \$PWD and \$H with given name, as saved by cdl."
+    echo "With '-f' also makes referenced paths exist before cd'ing."
     # Show list of saved variable sets
     path="$(get-config "var-save" -path)"
     echo "List of saved path names:"
     ls -m "$path"
 else
-    # Load them implicitly with appropriate commands.
-    cdh "$(cat "$path/H")"
-    pcd "$(cat "$path/PWD")"
+    # Set paths.
+    h="$(cat "$path/H")"
+    pwd="$(cat "$path/PWD")"
+    # Force existence if applicable
+    [ -n "$force" ] && mkdir -p "$pwd" "$h"
+    # Load paths indirectly via appropriate commands.
+    cdh "$h"
+    pcd "$pwd"
 fi
