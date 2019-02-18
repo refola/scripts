a18be84e2c2a88c23cc654e598435588b1a4c9df
Mark Haferkamp <refola@users.noreply.github.com>
2019-02-17 23:05:31 -0700

    make Zsh-compatible without breaking Bash
    Zsh defines $path as an array containing the contents of $PATH, for
    easier manipulation without having to splice on colons (and similar for
    other ALL_CAPS vs lower_case variable pairs). This commit changes that
    variable name to $vars_path, changes the shebang from "bash" to "sh",
    adds to the ShellCheck disable directive, and explains the extended
    directive.

--- shortcut/cdl.sh
+++ shortcut/cdl.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 ## cdl.sh
 # Sourcable script for `cdl()` function.
 
+## SC2039
+# I'm using a "POSIX" shebang so ShellCheck can help me avoid
+# Bash-isms so this works in Zsh, which does indeed support "local".
+## SC2168
 # This is sourced into a function, so "local" is valid.
-# shellcheck disable=SC2168
-local force path h pwd
+##
+# shellcheck disable=SC2039,SC2168
+local force vars_path h pwd
 
 if [ "$1" = "-f" ]; then
     force=true
@@ -12,20 +17,20 @@ if [ "$1" = "-f" ]; then
 fi
 
 # Get place to load variables from.
-path="$(get-config "var-save/$*" -path)"
+vars_path="$(get-config "var-save/$*" -path)"
 
-if [ -z "$1" ] || [ ! -e "$path" ]; then
+if [ -z "$1" ] || [ ! -e "$vars_path" ]; then
     echo "Usage: cdl [-f] name"
     echo "Restores \$PWD and \$H with given name, as saved by cdl."
     echo "With '-f' also makes referenced paths exist before cd'ing."
     # Show list of saved variable sets
-    path="$(get-config "var-save" -path)"
+    vars_path="$(get-config "var-save" -path)"
     echo "List of saved path names:"
-    ls -m "$path"
+    ls -m "$vars_path"
 else
     # Set paths.
-    h="$(cat "$path/H")"
-    pwd="$(cat "$path/PWD")"
+    h="$(cat "$vars_path/H")"
+    pwd="$(cat "$vars_path/PWD")"
     # Force existence if applicable
     [ -n "$force" ] && mkdir -p "$pwd" "$h"
     # Load paths indirectly via appropriate commands.
