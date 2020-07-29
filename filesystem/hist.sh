0b6ed4d0a242f99f45f1eba8b26a54172767873a
Mark Haferkamp <refola@users.noreply.github.com>
2020-07-29 04:38:27 -0700

    add --grep, --raw, and -- options to hist.sh

--- filesystem/hist.sh
+++ filesystem/hist.sh
@@ -1,15 +1,53 @@
-#!/bin/sh
+#!/bin/bash
 ## hist.sh
-# Searches through custom command history folder for commands matching
+# Search through custom command history folder for commands matching
 # given pattern, displaying last several results.
 
-dir="$HISTARCHIVE" # Custom history location used by $PROMPT_COMMAND
-count="${2-25}" # How many entries to show
-usage="Usage: $(basename "$0") regex [count]
+count=25 # default
+usage="$(basename "$0") [--] regex [count]
+$(basename "$0") [args ...]
 
-Searches files in $dir for given regex, returning the
-last count (default $count) lines. Count can be 'all' to display all
-matching history items."
-[ -z "$1" ] && echo "$usage" && exit 1 # Check for input
-count="$(echo "$count" | sed 's/^all$/+1/g')" # Make 'all' show all.
-grep -Ehe "$1" "$dir"/* | uniq | tail -n "$count" # Do the search
+Searches files in ${HISTARCHIVE/$HOME/\~} for given regex, returning the
+last 'count' (default $count) lines.
+
+Special args:
+all        When passed as a count, show all results.
+--         Force the next arg to be the regex (e.g., to search for '--raw')
+--grep -g  Only do the search, skipping filtration. Accepts additional
+           grep args.
+--raw  -r  Switch to raw grep mode without even the default
+           '-Ehe'. Further args are passed to grep as-is.
+"
+
+files=("$HISTARCHIVE"/*) # custom history location used by $PROMPT_COMMAND
+declare -a args=(--extended-regexp --no-filename) # grep arguments (default, before adding files list)
+filter=true # if the filter applies
+
+# arg parsing
+case "$1" in
+    "") # show usage when no args given
+        echo "$usage"
+        exit 1 ;;
+    --)
+        shift
+        args+=(--regexp="$1") ;;
+    --grep)
+        shift
+        filter=""
+        args+=("$@") ;;
+    --raw)
+        shift
+        filter=""
+        args=("$@") ;;
+    *) # just regex, no fancy args
+        args+=(--regexp="$1") ;;
+esac
+
+args+=("${files[@]}")
+if [ -n "$filter" ]; then
+    count="${2-$count}" # filter still on ⇒ $2 is count xor absent
+    count="${count/all/+1}" # convert non-numeric count to arg for 'tail'
+    grep "${args[@]}" | uniq | tail -n "$count"
+else
+    grep "${args[@]}"
+fi
